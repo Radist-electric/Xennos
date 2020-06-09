fd47af91656a4c97e4fc5e1c3b7ac69437e3ea0a
Radist <radist-electric@yandex.ru>
2020-06-09 21:02:34 +0500

    I've finished desktop version

--- js/main.js
+++ js/main.js
@@ -11,8 +11,33 @@ $(document).ready(function () {
         slidesToScroll: 1,
         // centerPadding: '40px',
         // variableWidth: false,
-        prevArrow: '<button class="arrow arrow_prev"><img src="img/portfolio/arrow-prev.png" alt="arrow prev"></button>',
-        nextArrow: '<button class="arrow arrow_next"><img src="img/portfolio/arrow-next.png" alt="arrow next"></button>',
+        prevArrow: '<button class="arrow"><img src="img/portfolio/arrow-prev.png" alt="arrow prev"></button>',
+        nextArrow: '<button class="arrow"><img src="img/portfolio/arrow-next.png" alt="arrow next"></button>',
+        responsive: [
+            {
+                breakpoint: 576,
+                settings: {
+                    arrows: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
+    });
+    $('.feedback__slider').slick({
+        dots: false,
+        centerMode: true,
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // centerPadding: '40px',
+        // variableWidth: true,
+        prevArrow: '<button class="arrow feedback__slider__arrow_prev"><img src="img/feedback/arrow-prev.png" alt="arrow prev"></button>',
+        nextArrow: '<button class="arrow feedback__slider__arrow_next"><img src="img/feedback/arrow-next.png" alt="arrow next"></button>',
         responsive: [
             {
                 breakpoint: 576,
