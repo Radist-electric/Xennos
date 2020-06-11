2415af072def167e896f7fb4467aafc50c816e7c
Radist <radist-electric@yandex.ru>
2020-06-11 20:35:51 +0500

    I've finished my work. It's necessary to test

--- js/main.js
+++ js/main.js
@@ -10,15 +10,25 @@ $(document).ready(function () {
         autoplaySpeed: 3000,
         slidesToShow: 3,
         slidesToScroll: 1,
-        prevArrow: '<button class="arrow"><img src="img/portfolio/arrow-prev.png" alt="arrow prev"></button>',
-        nextArrow: '<button class="arrow"><img src="img/portfolio/arrow-next.png" alt="arrow next"></button>',
+        prevArrow: '<button class="arrow portfolio__list__arrow portfolio__list__arrow_prev"><img src="img/portfolio/arrow-prev.png" alt="arrow prev"></button>',
+        nextArrow: '<button class="arrow portfolio__list__arrow portfolio__list__arrow_next"><img src="img/portfolio/arrow-next.png" alt="arrow next"></button>',
         responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    arrows: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    centerPadding: '0px'
+                }
+            }, 
             {
                 breakpoint: 576,
                 settings: {
                     arrows: true,
                     slidesToShow: 1,
                     slidesToScroll: 1,
+                    centerPadding: '15px'
                 }
             },
         ]
@@ -41,6 +51,7 @@ $(document).ready(function () {
                 breakpoint: 576,
                 settings: {
                     arrows: true,
+                    centerPadding: '0px'
                 }
             },
         ]
